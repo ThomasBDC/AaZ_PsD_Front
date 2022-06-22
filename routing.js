1f1d40fc0c56bf5c960d1d3b9d70834195bae453
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-06-22 15:10:35 +0200

    Finish js include for routing

--- routing.js
+++ routing.js
@@ -1,53 +1,90 @@
+const pageLoadEventName = "PageLoadEvent";
 const userIcon = document.getElementById('user-icon');
 const navbarBrand = document.querySelector('.navbar-brand');
-
-function loadFile(folder, fileName, target) {
-  var r = new XMLHttpRequest();
-
-  let url = '/' + folder + '/' + fileName + '.html';
-  r.open('GET', url, true);
-  r.onreadystatechange = function () {
-    if (r.readyState != 4 || r.status != 200) return;
-    document.getElementById(target).innerHTML = r.responseText;
-    handleLinkPwdForgotten();
-  };
-  r.send();
-}
+const loadingModal = new bootstrap.Modal('#LoadingModal', {
+  keyboard:false,
+  backdrop: 'static'
+})
 
 window.addEventListener('DOMContentLoaded', (event) => {
+  //loadingModal.show();
   let queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  // loadFile(urlParams.get('rep'), urlParams.get('fich'), 'pageContent');
-  // loadFile('theatres', 'alltheatres', 'pageContent');
-  // loadFile('connexion', 'connexion', 'pageContent');
-  homePage();
+
+  //Si j'ai des paramètres je récupère mon fichier
+  loadFile(urlParams.get('rep'), urlParams.get('fich'));
+
+  window.addEventListener(pageLoadEventName, (e) => {
+    const pageName = e.detail.folder+'_'+e.detail.fileName;
+    switch (pageName){
+        case "home_home":
+          homePage();
+          break;
+        case "connexion_connexion":
+          connexionPage();
+          break;
+        case "connexion_forget_pwd_connexion_forget_pwd":
+          pwdForgottenPage();
+          break;
+        default:
+          alert("Pas de script implémenté pour cette page");
+          break;
+    }
+  });
+  // setTimeout(() => {
+  //   loadingModal.hide();
+  // }, "500")
 });
 
 const connexionPage = () => {
-  loadFile('connexion', 'connexion', 'pageContent');
-  const headLink = document.querySelectorAll('head link');
-  headLink[headLink.length - 1].href = './connexion/connexion.css';
+  addCssToFile('connexion', 'connexion');
 };
 
 const homePage = () => {
-  loadFile('home', 'home', 'pageContent');
+  addCssToFile('home', 'home');
+};
+
+const pwdForgottenPage = () => {
+  addCssToFile('connexion_forget_pwd', 'connexion_forget_pwd');
+};
+
+//Pour ajouter du CSS à la page
+function addCssToFile(folder, fileName){
   // Create new link Element CSS + Set Attributes for Link CSS Element
   let link = document.createElement('link');
   link.rel = 'stylesheet';
   link.type = 'text/css';
-  link.href = './home/home.css';
+  link.href = './'+folder+'/'+fileName+'.css';
 
   document.querySelector('head').appendChild(link);
-};
 
-const pwdForgottenPage = () => {
-  loadFile('connexion_forget_pwd', 'connexion_forget_pwd', 'pageContent');
-};
+}
 
-const handleLinkPwdForgotten = () => {
-  pwdForgottenLink = document.getElementById('pwd_forgotten_link');
-  pwdForgottenLink.addEventListener('click', pwdForgottenPage);
-};
+//Pour charger un fichier HTML
+function loadFile(folder, fileName, target = "pageContent") {
 
-userIcon.addEventListener('click', connexionPage);
-navbarBrand.addEventListener('click', homePage);
+  if(folder == null || folder == undefined){
+    folder = "home";
+  }
+  if(fileName == null || fileName == undefined){
+    fileName = "home";
+  }
+
+  var r = new XMLHttpRequest();
+  let url = '/' + folder + '/' + fileName + '.html';
+  r.open('GET', url, true);
+  r.onreadystatechange = function () {
+    if (r.readyState != 4 || r.status != 200) return;
+    //J'ai chargé mon HTMl
+    document.getElementById(target).innerHTML = r.responseText;
+    //Mon HTML est visible sur la page
+    const pageLoad = new CustomEvent(pageLoadEventName, {
+      detail: {
+        folder: folder,
+        fileName: fileName
+      }
+    });
+    window.dispatchEvent(pageLoad);
+  };
+  r.send();
+}
