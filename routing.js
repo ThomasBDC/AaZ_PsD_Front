c7de91af9f03b1188a9debbc6e9f96f5b652ee00
Abdel Bailoul <ab.bailoul@gmail.com>
2022-06-22 12:31:49 +0200

    added home + connexion page

--- routing.js
+++ routing.js
@@ -1,18 +1,42 @@
-function loadFile(folder, fileName, target) {
-    var r = new XMLHttpRequest();
+const userIcon = document.getElementById('user-icon');
+const navbarBrand = document.querySelector('.navbar-brand');
 
-    let url = "/"+folder+"/"+fileName+".html";
-    r.open("GET", url, true);
-    r.onreadystatechange = function () {
-      if (r.readyState != 4 || r.status != 200) return;
-      document.getElementById(target).innerHTML = r.responseText;
-    };
-    r.send();
+function loadFile(folder, fileName, target) {
+  var r = new XMLHttpRequest();
+
+  let url = '/' + folder + '/' + fileName + '.html';
+  r.open('GET', url, true);
+  r.onreadystatechange = function () {
+    if (r.readyState != 4 || r.status != 200) return;
+    document.getElementById(target).innerHTML = r.responseText;
+  };
+  r.send();
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    
-    let queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    loadFile(urlParams.get("rep"), urlParams.get("fich"), "pageContent");
+window.addEventListener('DOMContentLoaded', (event) => {
+  let queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  // loadFile(urlParams.get('rep'), urlParams.get('fich'), 'pageContent');
+  // loadFile('theatres', 'alltheatres', 'pageContent');
+  // loadFile('connexion', 'connexion', 'pageContent');
+  homePage();
 });
+
+const connexionPage = () => {
+  loadFile('connexion', 'connexion', 'pageContent');
+  const headLink = document.querySelectorAll('head link');
+  headLink[headLink.length - 1].href = './connexion/connexion.css';
+};
+
+const homePage = () => {
+  loadFile('home', 'home', 'pageContent');
+  // Create new link Element CSS + Set Attributes for Link CSS Element
+  let link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = './home/home.css';
+
+  document.querySelector('head').appendChild(link);
+};
+userIcon.addEventListener('click', connexionPage);
+navbarBrand.addEventListener('click', homePage);
