90729537218d7acda77df5b66f271a62419bb678
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-07-13 14:38:26 +0200

    Ajout de la recherche utilisateur

--- users/users.js
+++ users/users.js
@@ -26,8 +26,59 @@ export function init(){
             `;
         });
         document.getElementById("userTableBody").innerHTML =tableBody;
+        setSearchOn();
         hideLoader();
 
       })
       .catch(error => console.log('error', error));
+}
+
+function setSearchOn(){
+  document.getElementById("btn-search-user").addEventListener("click", Search);
+}
+
+function Search(){
+  showLoader();
+  let search = document.getElementById("rechercheUser").value;
+  let myHeaders = new Headers();
+    let token = getToken();
+    myHeaders.append("Authorization", "Bearer "+token);
+  
+    let requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+  
+    fetch("https://aazpsd.azurewebsites.net/api/Users/Search?searchLabel="+search, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      let tableBody = '';
+      result.forEach(utilisateur => {
+          tableBody += `
+              <tr>
+                  <td>${utilisateur.surname}</td>
+                  <td>${utilisateur.forename}</td>
+                  <td>${utilisateur.mail}</td>
+                  <td>${utilisateur.telephone}</td>
+                  <td>${utilisateur.dateEmbauche}</td>
+                  <td>${utilisateur.role.name}</td>
+              </tr>
+          `;
+      });
+      if(tableBody == ''){
+        document.getElementById("userTableBody").innerHTML = `
+          <tr>
+            <td class="text-center text-danger" colspan="6">Aucun résultat</td>
+          </tr>`;
+      }
+      else{
+        document.getElementById("userTableBody").innerHTML = tableBody;
+      }
+      hideLoader();
+    })
+    .catch(error => {
+      console.log('error', error)
+      hideLoader();
+    });
 }
